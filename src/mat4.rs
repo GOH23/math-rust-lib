d85af189fc1d9b011913e2a60ff59b212fc598fb
GOH23 <padadamlpd@gmail.com>
2026-01-11 17:13:47 +0300

    added new methods to matrix

--- src/mat4.rs
+++ src/mat4.rs
@@ -299,24 +299,27 @@ impl Mat4 {
         let wy = w * y2;
         let wz = w * z2;
 
-        Self::new([
-            1.0 - (yy + zz),
-            xy + wz,
-            xz - wy,
-            0.0,
-            xy - wz,
-            1.0 - (xx + zz),
-            yz + wx,
-            0.0,
-            xz + wy,
-            yz - wx,
-            1.0 - (xx + yy),
-            0.0,
-            position.x,
-            position.y,
-            position.z,
-            1.0,
-        ].to_vec())
+        Self::new(
+            [
+                1.0 - (yy + zz),
+                xy + wz,
+                xz - wy,
+                0.0,
+                xy - wz,
+                1.0 - (xx + zz),
+                yz + wx,
+                0.0,
+                xz + wy,
+                yz - wx,
+                1.0 - (xx + yy),
+                0.0,
+                position.x,
+                position.y,
+                position.z,
+                1.0,
+            ]
+            .to_vec(),
+        )
     }
 
     pub fn from_position_rotation_to_ref(position: &Vec3, rotation: &Quat, result: &mut Mat4) {
@@ -364,7 +367,70 @@ impl Mat4 {
         values[14] = position.z; // m23
         values[15] = 1.0; // m33
     }
+    pub fn inverse(&self) -> Self {
+        let m = &self.values;
+        let mut out = [0.0; 16];
 
+        let a00 = m[0];
+        let a01 = m[1];
+        let a02 = m[2];
+        let a03 = m[3];
+        let a10 = m[4];
+        let a11 = m[5];
+        let a12 = m[6];
+        let a13 = m[7];
+        let a20 = m[8];
+        let a21 = m[9];
+        let a22 = m[10];
+        let a23 = m[11];
+        let a30 = m[12];
+        let a31 = m[13];
+        let a32 = m[14];
+        let a33 = m[15];
+
+        let b00 = a00 * a11 - a01 * a10;
+        let b01 = a00 * a12 - a02 * a10;
+        let b02 = a00 * a13 - a03 * a10;
+        let b03 = a01 * a12 - a02 * a11;
+        let b04 = a01 * a13 - a03 * a11;
+        let b05 = a02 * a13 - a03 * a12;
+        let b06 = a20 * a31 - a21 * a30;
+        let b07 = a20 * a32 - a22 * a30;
+        let b08 = a20 * a33 - a23 * a30;
+        let b09 = a21 * a32 - a22 * a31;
+        let b10 = a21 * a33 - a23 * a31;
+        let b11 = a22 * a33 - a23 * a32;
+
+        let det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+        if det.abs() < 1e-10 {
+            return Self::identity();
+        }
+
+        let inv_det = 1.0 / det;
+
+        out[0] = (a11 * b11 - a12 * b10 + a13 * b09) * inv_det;
+        out[1] = (a02 * b10 - a01 * b11 - a03 * b09) * inv_det;
+        out[2] = (a31 * b05 - a32 * b04 + a33 * b03) * inv_det;
+        out[3] = (a22 * b04 - a21 * b05 - a23 * b03) * inv_det;
+        out[4] = (a12 * b08 - a10 * b11 - a13 * b07) * inv_det;
+        out[5] = (a00 * b11 - a02 * b08 + a03 * b07) * inv_det;
+        out[6] = (a32 * b02 - a30 * b05 - a33 * b01) * inv_det;
+        out[7] = (a20 * b05 - a22 * b02 + a23 * b01) * inv_det;
+        out[8] = (a10 * b10 - a11 * b08 + a13 * b06) * inv_det;
+        out[9] = (a01 * b08 - a00 * b10 - a03 * b06) * inv_det;
+        out[10] = (a30 * b04 - a31 * b02 + a33 * b00) * inv_det;
+        out[11] = (a21 * b02 - a20 * b04 - a23 * b00) * inv_det;
+        out[12] = (a11 * b07 - a10 * b09 - a12 * b06) * inv_det;
+        out[13] = (a00 * b09 - a01 * b07 + a02 * b06) * inv_det;
+        out[14] = (a31 * b01 - a30 * b03 - a32 * b00) * inv_det;
+        out[15] = (a20 * b03 - a21 * b01 + a22 * b00) * inv_det;
+
+        Self::new(out.to_vec())
+    }
+    pub fn get_position(&self) -> Vec3 {
+        Vec3::new(self.values[12], self.values[13], self.values[14])
+    }
     pub fn set(mut self, val: Vec<f32>) {
         self.values = val;
     }
